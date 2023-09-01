5c2f8863d1683a75927ff1bb1809b705ddf645fa
Lewis <lewis@orderly.network>
2023-09-01 22:16:30 +0800

    update interface comments

--- contracts/interface/ILedgerCrossChainManager.sol
+++ contracts/interface/ILedgerCrossChainManager.sol
@@ -1,22 +1,26 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+// Importing types
 import "contract-evm/src/library/types/EventTypes.sol";
 import "contract-evm/src/library/types/AccountTypes.sol";
 
+/// @title ILedgerCrossChainManager Interface
+/// @notice Interface for managing cross-chain activities related to the ledger.
 interface ILedgerCrossChainManager {
-    // cross chain call deposit | from vault to leger
-    //function deposit(
-    //  AccountTypes.AccountDeposit memory message
-    //) external;
-
-    // cross chain withdraw approve | from leger to vault
+    /// @notice Approves a cross-chain withdrawal from the ledger to the vault.
+    /// @param data Struct containing withdrawal data.
     function withdraw(EventTypes.WithdrawData memory data) external;
-    // cross chain withdraw finish | from vault to leger
-    //function withdrawFinish(AccountTypes.AccountWithdraw memory message) external;
 
-    // admin call
+    /// @notice Sets the ledger address.
+    /// @param _ledger Address of the new ledger.
     function setLedger(address _ledger) external;
+
+    /// @notice Sets the operator manager address.
+    /// @param _operatorManager Address of the new operator manager.
     function setOperatorManager(address _operatorManager) external;
+
+    /// @notice Sets the cross-chain relay address.
+    /// @param _crossChainRelay Address of the new cross-chain relay.
     function setCrossChainRelay(address _crossChainRelay) external;
 }
--- contracts/interface/IVaultCrossChainManager.sol
+++ contracts/interface/IVaultCrossChainManager.sol
@@ -1,46 +1,37 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+// Importing necessary utility libraries and types
 import "../utils/OrderlyCrossChainMessage.sol";
 import "contract-evm/src/library/types/AccountTypes.sol";
 import "contract-evm/src/library/types/VaultTypes.sol";
 
+/// @title IVaultCrossChainManager Interface
+/// @notice Interface for managing cross-chain activities related to the vault.
 interface IVaultCrossChainManager {
-    /**
-     * @notice withdraw from ledger
-     * @param _withdraw withdraw data
-     */
+    /// @notice Triggers a withdrawal from the ledger.
+    /// @param _withdraw Struct containing withdrawal data.
     function withdraw(VaultTypes.VaultWithdraw memory _withdraw) external;
 
-    /**
-     * @notice deposit to vault
-     * @param _data deposit data
-     */
+    /// @notice Initiates a deposit to the vault.
+    /// @param _data Struct containing deposit data.
     function deposit(VaultTypes.VaultDeposit memory _data) external;
 
-    /**
-     * @notice deposit to vault with native fee
-     * @param _data deposit data
-     * @param _amount fee amount
-     */
+    /// @notice Initiates a deposit to the vault along with native fees.
+    /// @param _data Struct containing deposit data.
+    /// @param _amount Amount of native fee.
     function depositWithFee(VaultTypes.VaultDeposit memory _data, uint256 _amount) external payable;
 
-    /**
-     * @notice get deposit fee
-     * @param _data deposit data
-     * @return fee
-     */
+    /// @notice Fetches the deposit fee based on deposit data.
+    /// @param _data Struct containing deposit data.
+    /// @return fee The calculated deposit fee.
     function getDepositFee(VaultTypes.VaultDeposit memory _data) external view returns (uint256);
 
-    /**
-     * @notice set Vault
-     * @param _vault vault address
-     */
+    /// @notice Sets the vault address.
+    /// @param _vault Address of the new vault.
     function setVault(address _vault) external;
 
-    /**
-     * @notice set crossChainRelay
-     * @param _crossChainRelay crossChainRelay address
-     */
+    /// @notice Sets the cross-chain relay address.
+    /// @param _crossChainRelay Address of the new cross-chain relay.
     function setCrossChainRelay(address _crossChainRelay) external;
 }
